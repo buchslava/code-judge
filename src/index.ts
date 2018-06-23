bcfd900df89c1575945179460e1934dc895bf4c8
buchslava <vyacheslav.chub@gmail.com>
2018-06-23 19:20:33 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import * as fs from 'fs';
+import { includes } from 'lodash';
 import * as Levenstain from 'levenshtein';
 
 const esprima = require('esprima');
+
+export type BranchDescriptor = {
+  block;
+  parent;
+};
+
+export interface Rule {
+  processCodeTreeBranch(bd: BranchDescriptor);
+  fillIssues();
+}
 
 export function prepareTree(file: string) {
   const content = fs.readFileSync(file, 'utf-8');
@@ -27,4 +38,34 @@ export function walkCode(codeTree, onNewBranch: Function, onCodePassed: Function
   walkBranch(codeTree);
   onCodePassed();
   // console.log(JSON.stringify(codeTree, null, 2));
+}
+
+export class SimilarFunctionNames implements Rule {
+  issues = [];
+
+  private functionDecls = [];
+
+  processCodeTreeBranch(bd: BranchDescriptor) {
+    if (bd.block.type === 'FunctionDeclaration') {
+      this.functionDecls.push(bd.block.id.name);
+    }
+  }
+
+  fillIssues() {
+    for (let i = 0; i < this.functionDecls.length; i++) {
+      for (let j = 0; j < this.functionDecls.length; j++) {
+        if (
+          i !== j &&
+          !includes(this.issues, this.functionDecls[i]) &&
+          !includes(this.issues, this.functionDecls[j])
+        ) {
+          const l = new Levenstain(this.functionDecls[i], this.functionDecls[j]);
+
+          if (l.distance < 5) {
+            this.issues.push(this.functionDecls[i], this.functionDecls[j]);
+          }
+        }
+      }
+    }
+  }
 }
--- test/main.spec.ts
+++ test/main.spec.ts
@@ -1,36 +1,22 @@
 import * as chai from 'chai';
 import { includes } from 'lodash';
 import * as Levenstain from 'levenshtein';
-import { prepareTree, walkCode } from '../src/index';
+import { prepareTree, walkCode, SimilarFunctionNames, BranchDescriptor } from '../src/index';
 
 const expect = chai.expect;
 
-describe('main', () => {
-  it('foo', done => {
+describe('rules', () => {
+  it('SimilarFunctionNames', done => {
     const tree = prepareTree('./test/fixtures/similar.js');
+    const rule = new SimilarFunctionNames();
 
-    const functionDecls = [];
-
-    walkCode(tree, (branchDescriptor) => {
-      if (branchDescriptor.block.type === 'FunctionDeclaration') {
-        functionDecls.push(branchDescriptor.block.id.name);
-      }
+    walkCode(tree, (bd: BranchDescriptor) => {
+      rule.processCodeTreeBranch(bd);
     }, () => {
-      const issueFunction = [];
+      rule.fillIssues();
+      console.log(rule.issues);
 
-      for (let i = 0; i < functionDecls.length; i++) {
-        for (let j = 0; j < functionDecls.length; j++) {
-          if (i !== j && !includes(issueFunction, functionDecls[i]) && !includes(issueFunction, functionDecls[j])) {
-            const l = new Levenstain(functionDecls[i], functionDecls[j]);
-
-            if (l.distance < 5) {
-              issueFunction.push(functionDecls[i], functionDecls[j]);
-            }
-          }
-        }
-      }
-      
-      console.log(issueFunction);
+      expect(rule.issues).to.deep.equal(['getElementById', 'getElementsByIds']);
     });
 
     done();
